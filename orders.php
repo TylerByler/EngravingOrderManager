09bb43ce108592889394c0aaba4994214be34028
TylerByler <tylerbyler203@gmail.com>
2024-11-27 06:52:18 -0500

    new css file and refactor of orders page

--- orders.php
+++ orders.php
@@ -26,20 +26,27 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order List</title>
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/orders.css">
 </head>
 <body>
     <a href="index.php" class="homeButton">HOME</a>
     <div class="container flex-column justify-center text-center">
         <h1>Orders</h1>
         <!-- INSERT HTML USING RESULTS FROM DB -->
-         <div class="column-titles-container">
-            <div class="column-title left-title">INVOICE #</div>
-            <div class="column-title">PRODUCT #</div>
-            <div class="column-title">DESIGN #</div>
-            <div class="column-title right-title">FONT</div>
-         </div>
+        <div class="column-titles-container">
+            <div class="column-title left-title">Invoice #</div>
+            <div class="column-title">Salesperson</div>
+            <div class="column-title right-title">Date Created</div>
+        </div>
+        <a href=<?php ?> class="order-link">
+            <div class="order-container">
+                <div class="order-column">INVOICE_NUMBER</div>
+                <div class="order-column">SALESPERSON_NAME</div>
+                <div class="order-column">DATETIME</div>
+            </div>
+        </a>
          <?php
-            if (empty($results)) {
+            /* if (empty($results)) {
                 echo "No Results";
             } else {
                 foreach ($results as $row) {
@@ -48,7 +55,7 @@ try {
                     echo htmlspecialchars($row["salesperson_name"]);
                     echo "</div>";
                 }
-            }
+            } */
         ?>
     </div>
     
