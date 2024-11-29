33263c65b21b19276d8f14d59e0bf3682bbe38e1
TylerByler <tylerbyler203@gmail.com>
2024-11-29 14:57:53 -0500

    Making the code messier

--- entry.js
+++ entry.js
@@ -1,15 +1,41 @@
-var stgOneBtn = document.getElementById("stgOneBtn");
-var invoice_number;
-var salesperson_name;
+const stgOneBtn = document.getElementById("stgOneBtn");
+const stg1 = document.getElementById("stg1");
+const stg2 = document.getElementById("stg2");
+const invoice_header = document.getElementById("invoice-header");
+const salesperson_header = document.getElementById("salesperson-header");
+const invoice_number = document.getElementById("invoice_number");
+const salesperson_name = document.getElementById("salesperson_name");
+var invoice_number_temp;
+var salesperson_name_temp;
 
-console.log("invoice_number: ", invoice_number);
-console.log("salesperson_name: ", salesperson_name);
+
+console.log("invoice_number: ", invoice_number_temp);
+console.log("salesperson_name: ", salesperson_name_temp);
 
 stgOneBtn.addEventListener("click", () => {
-    invoice_number = document.getElementById("invoice_number").value;
-    salesperson_name = document.getElementById("salesperson_name").value;
-    
-    if (invoice_number != null || salesperson_name != null) {
-        
+    invoice_number_temp = document.getElementById("invoice_number_temp").value;
+    salesperson_name_temp = document.getElementById("salesperson_name_temp").value;
+
+    printValues();
+
+    if (invoice_number_temp == "" || salesperson_name_temp == "") {
+        // TELL USER INVALID INPUT
+        document.getElementById("invoice_number_temp").value = "";
+        document.getElementById("salesperson_name_temp").value = "";
+    } else {
+        stg1.classList.add("hidden");
+        stg2.classList.remove("hidden");
+
+        invoice_header.innerText = "Invoice #" + invoice_number_temp;
+        salesperson_header.innerText = "Salesperson: " + salesperson_name_temp;
+
+        invoice_number.value = invoice_number_temp;
+        salesperson_name.value = salesperson_name_temp;
     }
 });
+
+printValues = () => {
+    console.log("Print New Values")
+    console.log(invoice_number_temp.value);
+    console.log(salesperson_name_temp.value);
+}
--- entry.php
+++ entry.php
@@ -9,21 +9,69 @@
 </head>
 <body>
     <div class="container justify-center flex-column text-center">
+
         <h1>Entry Form</h1>
         <p class="reduceMargin">Use the form below to enter an order for engraving</p>
-        <div class="">
+
+        <div id="stg1" class="stg-wrapper">
             <div class="label-wrapper">
-                <label for="invoice_number">Invoice #
-                    <input id="invoice_number" type="text" name="invoice_number">
+                <label for="invoice_number_temp">Invoice #
+                    <input id="invoice_number_temp" type="text" name="invoice_number_temp">
                 </label>
             </div>
             <div class="label-wrapper">
-                <label for="salesperson_name">Salesperson
-                    <input id="salesperson_name" type="text" name="salesperson_name">
+                <label for="salesperson_name_temp">Salesperson
+                    <input id="salesperson_name_temp" type="text" name="salesperson_name_temp">
                 </label>
             </div>
-            <a id="stgOneBtn" class="btn">Send new entry</a>
+            <a id="stgOneBtn" class="btn">Create New Order</a>
         </div>
+
+        <div id="stg2" class="stg-wrapper hidden">
+            <div class="form-header">
+                <p id="invoice-header"></p>
+                <p id="salesperson-header"></p>
+            </div>
+
+            <form action="includes/formhandler.inc.php" method="post">
+
+                <input type="hidden" name="invoice_number" id="invoice_number">
+                <input type="hidden" name="salesperson_name" id="salesperson_name">
+                <div class="label-wrapper">
+                    <label for="product_number">Product #
+                        <input type="text" name="product_number" id="product_number">
+                    </label>
+                </div>
+                <div class="label-wrapper">
+                    <label for="product_desc">Product Description
+                        <input type="text" name="product_desc" id="product_desc">
+                    </label>
+                </div>
+                <div class="label-wrapper">
+                    <label for="color">Color
+                        <input type="text" name="color" id="color">
+                    </label>
+                </div>
+                <div class="label-wrapper">
+                    <label for="design_number">Design #
+                        <input type="text" name="design_number" id="design_number">
+                    </label>
+                </div>
+                <div class="label-wrapper">
+                    <label for="font">Font
+                        <input type="text" name="font" id="font">
+                    </label>
+                </div>
+                <div class="label-wrapper">
+                    <label for="engraving_desc">Engraving Description
+                        <input type="text" name="engraving_desc" id="engraving_desc">
+                    </label>
+                </div>
+                
+            </form>
+            <button>Submit New Order</button>
+        </div>
+
     </div>
     <div id="logobox"><img src="images/logo.png"></div>
     <a href="index.php" class="homeButton">HOME</a>
