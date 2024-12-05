5bbbfac89a8bb332f78599be5d8d0051faa93bcc
TylerByler <tylerbyler203@gmail.com>
2024-12-05 14:55:02 -0500

    MinorProgress

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -1,29 +1,29 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $invoice_number = $_POST["invoice_number"];
-    $salesperson_name = $_POST["salesperson_name"];
 
-    try {
-        require_once "dbh.inc.php";
+    for () {
+        try {
+            require_once "dbh.inc.php";
 
-        $query = "INSERT INTO orders (invoice_number, salesperson_name) VALUES (:invoice_number, :salesperson_name);";
+            $query = "INSERT INTO orders (invoice_number, salesperson_name) VALUES (:invoice_number, :salesperson_name);";
 
-        $stmt = $pdo->prepare($query);
+            $stmt = $pdo->prepare($query);
 
-        $stmt->bindParam(":invoice_number", $invoice_number);
-        $stmt->bindParam(":salesperson_name", $salesperson_name);
+            $stmt->bindParam(":invoice_number", $invoice_number);
+            $stmt->bindParam(":salesperson_name", $salesperson_name);
 
-        $stmt->execute();
+            $stmt->execute();
 
-        $pdo = null;
-        $stmt = null;
+            $pdo = null;
+            $stmt = null;
 
-        header("Location:../entry.php");
+            header("Location:../entry.php");
 
-        die();
-    } catch (PDOException $e) {
-        die("Query failed: ". $e->getMessage());
+            die();
+        } catch (PDOException $e) {
+            die("Query failed: ". $e->getMessage());
+        }
     }
 } else {
     header("Location:../index.php");
--- includes/test.inc.php
+++ includes/test.inc.php
@@ -6,6 +6,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     foreach( $_POST['product_number'] as $value ) {
         print $value;
     }
+    
+    foreach( $_POST['engraving_desc'] as $value ) {
+        if ($value == "") {
+            print "N/A";
+        }
+        print $value;
+    }
+
 } else {
     header("Location:../index.php");
 }
